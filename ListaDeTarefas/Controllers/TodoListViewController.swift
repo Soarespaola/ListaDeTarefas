6685769be848fe1c299760a6fddc71baa98a43a5
Paola Soares <paolaalcantara1@yahoo.com.br>
2022-02-06 21:28:36 -0300

    Adicionando itens nas categorias

--- ListaDeTarefas/Controllers/TodoListViewController.swift
+++ ListaDeTarefas/Controllers/TodoListViewController.swift
@@ -11,6 +11,13 @@ import CoreData
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
+    
+    var selectedCategory : Category? {
+        didSet{
+            
+            loadItems()
+        }
+    }
 
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
@@ -22,8 +29,7 @@ class TodoListViewController: UITableViewController {
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         
-        loadItems()
-//
+        
 //        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
 //
 //            itemArray = items
@@ -101,6 +107,7 @@ class TodoListViewController: UITableViewController {
             newItem.title = textField.text!
             
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             
             self.saveItems()
